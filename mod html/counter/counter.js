0d2e59be0aa9c3c665e2de888efc6d1c28d1ac66
John-MissionBit <johnnguyen1119@gmail.com>
2019-07-02 11:01:50 -0700

    added javascript to counter

--- mod html/counter/counter.js
+++ mod html/counter/counter.js
@@ -5,7 +5,23 @@ let value = 0
 document.querySelector('#value').innerHTML = value.toString()
 
 // 1. add a handler for the `subtract` button
-
+document.querySelector('#subtract').onclick = function(){
+    value --;
+    document.querySelector('#value').innerHTML = value;
+}
 // 2. add a handler for the `add` button
-
+document.querySelector('#add').onclick = function(){
+    value ++;
+    document.querySelector('#value').innerHTML = value;
+}
 // 3. "DRY" your code out by making a function
+function change(sign){
+    if(sign == "add") {
+        value++
+        document.querySelector("#value").innerHTML = value.toString();
+    }
+    else {
+        value--
+        document.querySelector("#value").innerHTML = value.toString();
+    }
+}
